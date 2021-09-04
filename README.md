159785491b94342d2f4b1d7d6e52b6ea031fbe8c
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-09-03 22:43:49 -0700

    Train and save

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ I use conda for my env. to have the right libs. I used python 3.6 for my tests. 
 ```bash
   pip install tensorflow=2.0
   pip install matplotlib
-  pip install numpy=1.16.1
+  pip install numpy==1.16.1
   pip install pandas
 ```
     
---
+++ text/textNeural.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+data = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
+
+print(train_data[0])
+
+word_index = data.get_word_index()
+
+word_index = {k: (v + 3) for k, v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post",
+                                                        maxlen=250)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+
+
+def decode_review(text):
+    return " ".join([reverse_word_index.get(i, "?") for i in text])
+
+
+#### COMMENT OUT IF NO NEED TO TRAIN
+model = keras.Sequential()
+model.add(keras.layers.Embedding(88000, 16)) #word vector for 16 dimensions
+model.add(keras.layers.GlobalAveragePooling1D()) #average it out
+model.add(keras.layers.Dense(16, activation="relu")) #put into 16 layers
+model.add(keras.layers.Dense(1, activation="sigmoid")) #assign to single neuron
+
+model.summary()
+
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val,y_val), verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+print(results)
+
+model.save("model.h5")
+
+model = keras.models.load_model("model.h5")
+#### COMMENT OUT IF NO NEED TO TRAIN
+
+def review_encode(s):
+    encoded = [1]
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+
+    return encoded
+
+
+with open("test.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\"", "").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250)  # make the data 250 words long
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
