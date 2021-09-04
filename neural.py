85fde6ef20fc5dff28546a2f6dd4b57c0b7170e8
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-09-03 21:07:14 -0700

    testing

--- neural.py
+++ neural.py
@@ -12,5 +12,17 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-plt.imshow(train_images[4], cmap=plt.cm.binary)
-plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+    ])
+
+model.compile(optimizier="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Acc:", test_acc)
